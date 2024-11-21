e9a2eb2e1c3d9685433e2d75617e8f7bc925fde7
Aswinshaji0001 <aswinshaji0001@gmail.com>
2024-11-21 09:01:04 +0530

    Added

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,12 +1,8 @@
 import mongoose, { mongo } from "mongoose";
 
 const userSchema=new mongoose.Schema({
-    name:{type:String},
-    salary:{type:Number},
-    experience:{type:String},
-    designation:{type:String},
-    phone:{type:Number},
+    username:{type:String},
+    password:{type:String},
     email:{type:String},
-    profile:{type:String}
 });
 export default mongoose.model.user||mongoose.model("user",userSchema);
